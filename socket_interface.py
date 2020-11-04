f70535a534d4cd4fa1da20d4bf6163f6d5048c70
Davide Montagno Bozzone <40762415+DavideMontagno@users.noreply.github.com>
2020-11-04 18:29:25 +0100

    Commands work perfectly!

--- socket_interface.py
+++ socket_interface.py
@@ -5,22 +5,43 @@ import datetime
 
 HOST = "margot.di.unipi.it"
 PORT = 8421
-NAME_GAME = "ai9_"+datetime.datetime.now().strftime("%Y%m%d_%H%M%S")+"/"
+NAME_GAME = "ai9_"+datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 print(NAME_GAME)
-tn = telnetlib.Telnet(HOST, PORT)
-tn.write(bytes("NEW "+NAME_GAME+"\n", 'utf-8')) #this worka!
-time.sleep(0.5)
-print(tn.read_until(b"\n",550))
-print("I'm here")
-tn.write(bytes(NAME_GAME+" JOIN maDave H - fff\n", 'utf-8')) #this worka!
-print("Writed join command")
-print(tn.read_until(b"\n",550))
-time.sleep(0.5)
-tn.write(bytes(NAME_GAME+" LOOK\n","utf-8"))
-print(str(tn.read_until(b"\xc2\xbb",550).decode("utf-8") ))
-time.sleep(0.5)
-tn.write(bytes(NAME_GAME+" START\n","utf-8"))
-print(str(tn.read_until(b"\n",550)))
-time.sleep(0.5)
-tn.write(bytes(NAME_GAME+" SHOOT E\n","utf-8"))
-print(str(tn.read_until(b"\n",550)))
+tn_server = telnetlib.Telnet(HOST, PORT)
+tn_player = []
+time_sleep=0.51
+num_players = 7
+time.sleep(time_sleep)
+tn_server.write(bytes("NEW "+NAME_GAME+"\n", 'utf-8')) #this worka!
+time.sleep(time_sleep)
+tn_server.write(bytes(NAME_GAME+" LOOK\n","utf-8"))
+print(str(tn_server.read_until(b"\xc2\xbb\n",time_sleep).decode("utf-8") ))
+
+
+for i in range(0,num_players):
+    tn_player.append(telnetlib.Telnet(HOST, PORT))
+    tn_player[i].write(bytes(NAME_GAME+" JOIN player_"+str(i)+" H - fff\n", 'utf-8')) #this worka!
+    print(str(tn_player[i].read_until(b"\n",time_sleep).decode("utf-8") ))
+    time.sleep(time_sleep)
+
+
+
+time.sleep(time_sleep)
+tn_server.write(bytes(NAME_GAME+" START\n","utf-8"))
+print(str(tn_server.read_until(b"\n",time_sleep).decode("utf-8")))
+
+time.sleep(time_sleep)
+tn_player[0].write(bytes(NAME_GAME+" STATUS\n","utf-8"))
+print(str(tn_player[0].read_until(b"\xc2\xbb\n",time_sleep).decode("utf-8") ))
+
+
+time.sleep(time_sleep)
+tn_player[0].write(bytes(NAME_GAME+" LOOK\n","utf-8"))
+print(str(tn_player[0].read_until(b"\xc2\xbb\n",time_sleep).decode("utf-8") ))
+time.sleep(time_sleep)
+tn_player[0].write(bytes(NAME_GAME+" MOVE E\n","utf-8"))
+print(str(tn_player[0].read_until(b"\n",time_sleep).decode("utf-8")))
+time.sleep(time_sleep)
+tn_player[0].write(bytes(NAME_GAME+" LOOK\n","utf-8"))
+print(str(tn_player[0].read_until(b"\xc2\xbb\n",time_sleep).decode("utf-8") ))
+
