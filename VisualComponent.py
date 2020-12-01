afe20cddea256327753934386a2e19d5174f954e
vlnraf <raffaele.villani17@gmail.com>
2020-12-01 12:08:43 +0100

    refactor the server interface

--- VisualComponent.py
+++ VisualComponent.py
@@ -47,39 +47,6 @@ class VisualComponent():
 
         self.player_position = (int(y1+y2), int(x1+x2))
 
-
-    def updateGrid(self):
-        self.raw_map = self.player.process_map()
-
-        self.grid_cellular_map = Grid(
-            width=len(self.raw_map), height=len(self.raw_map[0]))
-
-        for row in range(len(self.raw_map)):
-            for column in range(len(self.raw_map[0])):
-
-                current_cell = self.raw_map[row][column]
-                walkable = True
-                if(current_cell == "#" or current_cell == "@" or current_cell == "!" or current_cell == "&" or current_cell == self.game_symbol.swapcase()):
-                    result = -1
-                    walkable = False
-                elif(current_cell == "$"):
-                    # distance.cityblock(self.flag,[row,column]).astype(int) -1
-                    result = 4
-                    walkable = True
-                elif(current_cell == self.flag):
-                    result = 1
-                    walkable = True
-                else:
-                    # distance.cityblock(self.flag,[row,column]).astype(int)
-                    result = 5
-                    walkable = True
-
-                self.grid_cellular_map.nodes[column][row] = Node(
-                    x=row, y=column, walkable=walkable, weight=result)
-
-        return self.grid_cellular_map
-
-
     def findStrategy(self):
         pass
 
