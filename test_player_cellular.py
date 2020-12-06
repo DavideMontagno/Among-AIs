132991484df17dfc0fea4a7e68722643818c1948
Davide Montagno Bozzone <40762415+DavideMontagno@users.noreply.github.com>
2020-12-06 19:05:20 +0100

    Update test

--- test_player_cellular.py
+++ test_player_cellular.py
@@ -11,7 +11,6 @@ import multiprocessing
 count=0
 png_gif_dir = "./gif/"
 tot=1
-debug = False
 
 
 def start_game(cellular_a):
@@ -21,107 +20,57 @@ def start_chat(cellular_chat):
     pass
 
 if __name__ == "__main__":
-    
-    #CREATION
-    NAME_GAME = "ai9_"+datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    print(NAME_GAME)
+    n_player = 3
+    participate_tournament = False
+    debug=False
+    tournament = "SmartCUP"
+    manager_game = ""
+    threads_player = []
+    threads_chat = []
+    NAME_GAME = "ai9_deduction84"#+datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     png_dir = str(NAME_GAME)
+    creation_game=False
+    created=False
+    if(participate_tournament):
+        for i in range(0,n_player):
+            Tournament(name="ai9_pl"+str(i+1), tournament=tournament)
+        input('Completed registring all players in tournament: '+str(tournament))
 
-    pl1=GameInterface(NAME_GAME,NAME_GAME,"ai9_pl1",player_descr="v0.1", flags="Q1")
-    print(pl1.manage_game("new"))
-    #JOIN GAME
-    print(pl1.interact("join"))
-    pl1.command_chat("name")
-    pl1.command_chat("join")
+    for i in range(0,n_player):
+        temp = GameInterface(NAME_GAME,NAME_GAME,"ai9_pl"+str(i+1),player_descr="v0.1")
 
-    pl2=GameInterface(NAME_GAME,NAME_GAME,"ai9_pl2",player_descr="v0.1")
-    print(pl2.interact("join"))
-    pl2.command_chat("name")
-    pl2.command_chat("join")
+        if(creation_game):
+            result = temp.manage_game("new").lower()
+            print(result)
+            if(result.find("error")!=-1):
+                print("ERRORE CREAZIONE")
+                exit()
+            else: 
+                creation_game=False
+                created=True
+                manager_game = temp
+        print(temp.interact("join"))
+        temp.command_chat("name")
+        temp.command_chat("join")
+        manager = multiprocessing.Manager()
+        manager_dict = manager.dict()
+        ca = CellularAutomata(temp, manager_dict,debug=debug) # to Debug
+        ca_chat = CellularAutomata_chat(temp, manager_dict,debug=False)
+        t_player = multiprocessing.Process(target=start_game, args=(ca,))
+        chat_player = multiprocessing.Process(target=start_chat, args=(ca_chat,))
+        threads_player.append(t_player)
+        threads_chat.append(chat_player)
+    if(created):
+        manager_game.manage_game("start")
+        print("Game started...")
+        
+    for n in range(len(threads_chat)):
+        threads_chat[n].start()
 
-    #print(pl2.chat("join", chat_name=NAME_GAME))
-    pl3=GameInterface(NAME_GAME,NAME_GAME,"pl3",player_descr="")
-    print(pl3.interact("join"))
-    #print(pl3.chat("join", chat_name=NAME_GAME))
-
-    pl4=GameInterface(NAME_GAME,NAME_GAME,"pl4",player_descr="")
-    print(pl4.interact("join"))
-    #print(pl4.chat("join", chat_name=NAME_GAME))
-
-    pl5=GameInterface(NAME_GAME,NAME_GAME,"pl5",player_descr="")
-    print(pl5.interact("join"))
-    #print(pl5.chat("join", chat_name=NAME_GAME))
-
-    pl6=GameInterface(NAME_GAME,NAME_GAME,"pl6",player_descr="")
-    print(pl6.interact("join"))
-    #print(pl6.chat("join", chat_name=NAME_GAME))
-
-    pl7=GameInterface(NAME_GAME,NAME_GAME,"pl7",player_descr="")
-    print(pl7.interact("join"))
-    #print(pl7.chat("join", chat_name=NAME_GAME))
-
-    pl8=GameInterface(NAME_GAME,NAME_GAME,"pl8",player_descr="")
-    print(pl8.interact("join"))
-    #print(pl8.chat("join", chat_name=NAME_GAME))
-
-    # START GAME
+    for n in range(len(threads_player)):
+        threads_player[n].start()
+    for n in range(len(threads_player)):
+        threads_player[n].join()
+    for n in range(len(threads_chat)):
+        threads_chat[n].terminate()
     
-
-    input('wait')
-    if(pl1.manage_game("start").lower().find("error")!=-1):
-        print("ERRORE CREAZIONE")
-        exit()
-    else: print("Started")
-    #print(pl1.status("look"))
-
-
-    #ca=CellularAutomata(pl1)
-    manager = multiprocessing.Manager()
-    manager_dict = manager.dict()
-
-    ca = CellularAutomata(pl1, manager_dict,debug=True) # to Debug
-    ca_chat = CellularAutomata_chat(pl1, manager_dict,debug=False)
-    #result = ca.play()
-    
-    t1 = multiprocessing.Process(target=start_game, args=(ca,))
-    c1 = multiprocessing.Process(target=start_chat, args=(ca_chat,))
-    '''
-    ca2=CellularAutomata(pl2)
-    ca_chat2 = CellularAutomata_chat(pl2)
-    t2 = multiprocessing.Process(target=start_game, args=(ca2,))
-    c2 = multiprocessing.Process(target=start_chat, args=(ca_chat2,))
-    ca3=CellularAutomata(pl3)
-    t3 = multiprocessing.Process(target=start_game, args=(ca3,))
-    ca4=CellularAutomata(pl4)
-    t4 = multiprocessing.Process(target=start_game, args=(ca4,))
-    ca5=CellularAutomata(pl5)
-    t5 = multiprocessing.Process(target=start_game, args=(ca5,))
-    ca6=CellularAutomata(pl6)
-    t6 = multiprocessing.Process(target=start_game, args=(ca6,))
-    ca7=CellularAutomata(pl7)
-    t7 = multiprocessing.Process(target=start_game, args=(ca7,))
-    ca8=CellularAutomata(pl8)
-    t8 = multiprocessing.Process(target=start_game, args=(ca8,))
-    ca9=CellularAutomata(pl9)
-    t9 = multiprocessing.Process(target=start_game, args=(ca9,))
-    ca10=CellularAutomata(pl10)
-    t10 = multiprocessing.Process(target=start_game, args=(ca10,))
-    ca11=CellularAutomata(pl11)
-    t11 = multiprocessing.Process(target=start_game, args=(ca11,))
-    ca12=CellularAutomata(pl12)
-    t12 = multiprocessing.Process(target=start_game, args=(ca12,))
-    ca13=CellularAutomata(pl13)
-    t13 = multiprocessing.Process(target=start_game, args=(ca13,))
-    ca14=CellularAutomata(pl14)
-    t14 = multiprocessing.Process(target=start_game, args=(ca14,))
-    ca15=CellularAutomata(pl15)
-    t15 = multiprocessing.Process(target=start_game, args=(ca15,))
-    '''
-    threads = [t1,c1]# , c1] ,t2,c2],t2,t3,t4,t5,t6,t7,t8]#,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,c1]
-    #threads = [t1,c1]
-    for n in range(len(threads)):
-        threads[n].start()
-    # Wait all threads to finish.
-    t1.join()
-    name="work"
-    c1.terminate()
