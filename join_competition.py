2241d1ea43f47d16e1606e5f0ce97e212acb0ef9
vlnraf <raffaele.villani17@gmail.com>
2020-12-09 14:25:56 +0100

    join cometition update

--- join_competition.py
+++ join_competition.py
@@ -1,4 +1,4 @@
-from player import Player
+from GameInterface import GameInterface
 from CellularAutomata import CellularAutomata
 from CellularAutomata_chat import CellularAutomata_chat
 from Tournament import Tournament
@@ -26,6 +26,7 @@ if __name__ == "__main__":
     pltournament3 = Tournament(name="ai9_pl3", tournament=tournament)
     input('Finished to join in tournament correctly! Press any key to continue..')
     ##start with game
+    manager = multiprocessing.Manager()
     for i in range(1, 13):
         value=tournament+"-"+str(i)+"-1"
         print("Start game on: "+value)
@@ -37,30 +38,33 @@ if __name__ == "__main__":
         debug_chat = False
 
 
-        pl1=Player(game_name=NAME_GAME,chat_name=NAME_CHAT,player_name="ai9_pl1",player_descr="ai9_1.0")
+        pl1=GameInterface(game_name=NAME_GAME,chat_name=NAME_CHAT,player_name="ai9_pl1",player_descr="ai9_1.0")
         print(pl1.interact("join"))
-        ca=CellularAutomata(pl1,debug=debug)
-        ca_chat = CellularAutomata_chat(pl1,debug=debug_chat)
+        ai_dict = manager.dict()
+        ca=CellularAutomata(pl1,ai_dict,debug=debug,mode='007')
+        ca_chat = CellularAutomata_chat(pl1,ai_dict,debug=debug_chat)
         pl1.command_chat("join")
         t_pl1 = multiprocessing.Process(target=start_game, args=(ca,))
         c_pl1 = multiprocessing.Process(target=start_chat, args=(ca_chat,))
 
         
-        pl2=Player(game_name=NAME_GAME,chat_name=NAME_CHAT,player_name="ai9_pl2",player_descr="ai9_1.0")
+        pl2=GameInterface(game_name=NAME_GAME,chat_name=NAME_CHAT,player_name="ai9_pl2",player_descr="ai9_1.0")
         print(pl2.interact("join"))
-        ca2=CellularAutomata(pl2,debug=debug)
-        ca2_chat = CellularAutomata_chat(pl2,debug=debug_chat)
+        ai_dict = manager.dict()
+        ca2=CellularAutomata(pl2,ai_dict,debug=debug,mode='kamikaze')
+        ca2_chat = CellularAutomata_chat(pl2,ai_dict,debug=debug_chat)
         t_pl2 = multiprocessing.Process(target=start_game, args=(ca2,))
         c_pl2 = multiprocessing.Process(target=start_chat, args=(ca2_chat,))
         pl2.command_chat("name")
         pl2.command_chat("join")
 
         
-        pl3=Player(game_name=NAME_GAME,chat_name=NAME_CHAT,player_name="ai9_pl3",player_descr="ai9_1.0")
+        pl3=GameInterface(game_name=NAME_GAME,chat_name=NAME_CHAT,player_name="ai9_pl3",player_descr="ai9_1.0")
         
         print(pl3.interact("join"))
-        ca3=CellularAutomata(pl3,debug=debug)
-        ca3_chat = CellularAutomata_chat(pl3,debug=debug_chat)
+        ai_dict = manager.dict()
+        ca3=CellularAutomata(pl3,ai_dict,debug=debug, mode='kamikaze')
+        ca3_chat = CellularAutomata_chat(pl3,ai_dict,debug=debug_chat)
         t_pl3 = multiprocessing.Process(target=start_game, args=(ca3,))
         c_pl3 = multiprocessing.Process(target=start_chat, args=(ca3_chat,))
         pl3.command_chat("name")
@@ -82,11 +86,15 @@ if __name__ == "__main__":
         t_pl2.join()
         t_pl3.join()
 
+        c_pl1.join()
+        c_pl2.join()
+        c_pl3.join()
 
 
-        c_pl1.terminate()
-        c_pl2.terminate()
-        c_pl3.terminate()
+
+#       c_pl1.terminate()
+#       c_pl2.terminate()
+#       c_pl3.terminate()
         #pl1.command_chat("leave")
         #pl2.command_chat("leave")
         #pl3.command_chat("leave")
